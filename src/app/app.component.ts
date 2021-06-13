787d1e585ad29ae8dd9ac8cb2fe5594be07aab0c
MasterDev <juanpb.96@gmail.com>
2021-06-12 19:13:47 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+
+  /* Pipes won't mutate the data just modify its display on the view */  
+  name: string = 'juan bonilla';
+  value: number = 1000;
+  obj = {
+    name : 'juan bonilla'
+  }
+
+  showName() {
+    console.log(this.name);
+    console.log(this.value);
+    console.log(this.obj);
+  }
 }
